0823f0da10709dc76cebc2f64d4c3cfd155577a3
Max-Hu <huzhiwise@gmail.com>
2016-03-14 20:37:56 +0800

    remove useless functions

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -37,39 +37,6 @@ function getAll(collection, globalCallback){
     return itemsList;
 }
 
-function queryObjectByBarcode(collection,barcode){
-    console.log(collection);
-    var result = {};
-    var myDocument = db.collection(collection).findOne({ "barcode": barcode });
-    console.log(myDocument);
-    if (myDocument) {
-        var myName = myDocument.name;
-
-        console.log(myName);
-    }
-    //var findObject = function(db, callback,execute) {
-    //    var cursor =db.collection(collection).find( { "barcode": barcode } );
-    //    cursor.each(function(err, doc) {
-    //        assert.equal(err, null);
-    //        if (doc != null) {
-    //            console.log('#######');
-    //            console.log(doc);
-    //            execute(doc);
-    //        } else {
-    //            callback();
-    //        }
-    //    });
-    //};
-    //MongoClient.connect(url, function(err, db) {
-    //    assert.equal(null, err);
-    //    findObject(db, function() {
-    //        db.close();
-    //    });
-    //}, function(doc){
-    //    globalCallback(doc);
-    //});
-}
-
 
 function insertData(collection,objects){
     insertDocument = function(db, callback) {
@@ -101,46 +68,7 @@ function removeData(collection,insertData){
 }
 
 
-function deleteObjectByBarcode(collection,barcode){
-
-    var removeObject = function(db, callback) {
-        db.collection(collection).deleteOne(
-            { "barcode": barcode },
-            function(err, results) {
-                console.log(results);
-                callback();
-            }
-        );
-    };
-
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        removeObject(db, function() {
-            db.close();
-        });
-    });
-}
-
-function updateObject(collection,object){
-    var update = function(db, callback) {
-        db.collection(collection).updateOne( object,
-            function(err, results) {
-                console.log(results);
-                callback();
-            });
-    };
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        update(db, function() {
-            db.close();
-        });
-    });
-}
-
 exports.initDB = initDB;
 exports.getAll = getAll;
 exports.insertData = insertData;
-exports.queryObjectByBarcode = queryObjectByBarcode;
-exports.deleteObjectByBarcode = deleteObjectByBarcode;
-exports.updateObject = updateObject;
 exports.removeData = removeData;
