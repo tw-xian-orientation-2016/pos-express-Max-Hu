dbe6c4ceccb3c039e174ce202c0dc3cd05d00e70
Max-Hu <huzhiwise@gmail.com>
2016-03-16 15:38:31 +0800

    crate receipt list api

--- routes/receiptList.js
+++ routes/receiptList.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
     res.render('receiptList', { title: 'ejs' });
 });
 
+router.get('/receipt/', function(req, res, next) {
+    var collection = req.body.collection;
+    mongodb.getAll(collection, function(allItems) {
+        res.send(allItems);
+    })
+});
+
+router.post('/', function(req, res, next) {
+    var cartItems = JSON.parse(req.body.cartItems);
+    mongodb.insertData('cart',cartItems);
+    res.send('success');
+});
+
 module.exports = router;
